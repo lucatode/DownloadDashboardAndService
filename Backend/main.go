7b832e6e3490847b58f84ec3b93effba70855423
DESKTOP-GFAGTVL\Luca <l.todeschini88@gmail.com>
2017-09-10 11:37:14 +0200

    Func in main for handling GET /downloads

--- Backend/main.go
+++ Backend/main.go
@@ -1,7 +1,113 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
+	goji "goji.io"
+	"goji.io/pat"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func main() {
+	connectionString := "localhost"
+	session, err := mgo.Dial(connectionString)
+
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	http.ListenAndServe("localhost:8000", mux(session, "AppDashboard", "Downloads"))
 }
 
 ///Mux function
+func mux(s *mgo.Session, db string, collection string) *goji.Mux {
+	mux := goji.NewMux()
+	mux.HandleFunc(pat.Get("/downloads"), allDownloads(s, db, collection))
+	return mux
+}
+
+///Route handling functions
+//This function handle '/downloads' route GET calls - Retriving all downloads in the db
+func allDownloads(s *mgo.Session, db string, collection string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//Query the db
+		downloads, err := allDownloadsQuery(s, db, collection)
+
+		//Evaluate errors
+		if err != nil {
+			//Handle response with json - Input: writer, message, code
+			ErrorWithJSON(w, "Replace with message", http.StatusInternalServerError)
+			//Log the error
+			log.Println("Failed to get all books", err)
+			//Exit
+			return
+		}
+
+		//Input: dbQueryResponse, prefix, indent
+		respBody, err := json.MarshalIndent(downloads, "", " ")
+
+		//Check error on Marshaling
+		if err != nil {
+			log.Fatal(err)
+		}
+		//Handle response with json - Input: writer, jsonBody, status
+		ResponseWithJSON(w, respBody, http.StatusOK)
+
+	}
+}
+
+///DB handling functions
+func allDownloadsQuery(s *mgo.Session, db string, collection string) ([]Download, error) {
+	//Get a new session
+	session := s.Copy()
+	defer session.Close()
+
+	c := session.DB(db).C(collection)
+
+	var downloads []Download
+	err := c.Find(bson.M{}).All(&downloads)
+
+	if err != nil {
+		fmt.Println("Some errors during find all")
+		return downloads, fmt.Errorf("Some errors during find all")
+	}
+
+	if len(downloads) == 0 {
+		fmt.Println("No documents in the db")
+		return downloads, fmt.Errorf("No documents in the db")
+	}
+
+	return downloads, nil
+}
+
+///Utilities
+// This function handle the response with json whenever an error occurs
+func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
+
+	//Set Writer header with Key and valueString
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	//Put the return code in the header
+	w.WriteHeader(code)
+
+	//Write on stream the message with the http.ResponseWriter interface
+	fmt.Fprintf(w, "{message: %q}", message)
+}
+
+// This function handle the response with json whenever an object is returned correctly from db query
+func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
+
+	//Set Writer header with Key and valueString
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	//Put the return code in the header
+	w.WriteHeader(code)
+
+	//Write the message with ResponseWriter
+	w.Write(json)
+}
